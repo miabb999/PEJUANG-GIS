1f592a9e07c31b70d694258382769e376e8b754a
NUR ARKHAMIA BATUBARA <37619556+miabb999@users.noreply.github.com>
2018-10-19 18:00:29 +0700

    Update 1.py

--- 1164049_NURARKHAMIABATUBARA/1.py
+++ 1164049_NURARKHAMIABATUBARA/1.py
@@ -1,14 +1,14 @@
-import shapefile 
-w=shapefile.Writer() 
-w.shapeType 
+import shapefile         # Mengimport modul shape file
+w=shapefile.Writer() 	   # Mendeklarasi variabel
+w.shapeType 			       # Menjalankan perintah dari pendeklarasian variabel
 
-w.field("kolom1","C") 
-w.field("kolom2","C") 
+w.field("kolom1","C")    # Membuat kolom dengan tipe data character
+w.field("kolom2","C")    # Membuat kolom dengan tipe data character
 
-w.record("ngek","satu") 
-w.record("ngok","dua") 
+w.record("ngek","satu")  # Mengisi record ke kolom yang sudah di buat tadi
+w.record("ngok","dua")   # Mengisi record ke kolom yang sudah di buat tadi
 
-w.point(1,1) 
-w.point(2,2) 
+w.point(1,1) 			       # Membuat point/titik 
+w.point(2,2) 		         # Membuat point/titik 
 
-w.save("soal1") 
+w.save("soal1") 		     # Untuk save menjadi shp file
