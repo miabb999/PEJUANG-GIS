18c17cd2d928094a225a6e9faaa6d8e56a350389
Widary Anggraini Mindo V Siahaan <widary.siahaan2804@gmail.com>
2018-10-20 09:02:42 +0700

    soal3 dan 4

---
+++ WIDARY ANGGRAINI MINDO V SIAHAAN_1164057/soal 3/soal 3.py
@@ -0,0 +1,12 @@
+import shapefile    #nantinya modul shape file akan di import
+w=shapefile.Writer(shapeType=1) #mendeklarasikan variable
+w.shapeType #menjalankan perintah dari sih variable
+w.shapeType=3   #menjalankan perintah dari sih variable
+w.shapeType #menjalankan perintah dari sih variable
+w.field("kolom1","C")   ##membuat kolom dengan tipe data karakter
+w.field("kolom2","C")   ##membuat kolom dengan tipe data karakter
+w.record("ngek","satu") #mengisi record pada kolom yang dibuat tadi
+w.record("ngok","dua")  #mengisi record pada kolom yang dibuat tadi
+w.point(1,1)    #membuat point atau titik
+w.point(2,2)    #membuat point atau titik
+w.save("soal3") #untuk menyimpan dengan format file.shp
