12dd560676bd402109c43cf37777b09bf832dbb6
widarywidary <33123078+widarywidary@users.noreply.github.com>
2018-10-22 15:45:49 +0700

    Update soal 5.py

--- WIDARY ANGGRAINI MINDO V SIAHAAN_1164057/soal 5/soal 5.py
+++ WIDARY ANGGRAINI MINDO V SIAHAAN_1164057/soal 5/soal 5.py
@@ -1,8 +1,8 @@
-import shapefile
-w=shapefile.Writer()
-w.shapeType
-w.field("kolom1","C")
-w.field("kolom2","C")
-w.record("ngek","satu")
-w.line(parts=[[[1,5],[5,5],[5,1],[3,3],[1,1]]])
-w.save("soal5")
+import shapefile #nantinya modul shape file akan di import
+w=shapefile.Writer() #mendeklarasikan variable
+w.shapeType  #menjalankan perintah dari sih variable
+w.field("kolom1","C") ##membuat kolom dengan tipe data karakter
+w.field("kolom2","C")##membuat kolom dengan tipe data karakter
+w.record("ngek","satu") #mengisi record pada kolom yang dibuat tadi
+w.line(parts=[[[1,5],[5,5],[5,1],[3,3],[1,1]]]) #membuat point atau titik atau garis
+w.save("soal5") #untuk menyimpan dengan format file.shp
