e1abc55d4fbf5fdd2ead37e5034b215104296384
NUR ARKHAMIA BATUBARA <37619556+miabb999@users.noreply.github.com>
2018-10-22 20:32:28 +0700

    Add files via upload

---
+++ 1164049_NURARKHAMIABATUBARA/6.py
@@ -0,0 +1,25 @@
+import shapefile 		# Mengimport modul shape file
+w=shapefile.Writer() 	# Mendeklarasi variabel
+w.shapeType 			# Menjalankan perintah dari pendeklarasian variabel
+
+w.field("kolom1","C") 	# Membuat kolom dengan tipe data character
+w.field("kolom2","C") 	# Membuat kolom dengan tipe data character
+
+w.record("ngek","satu") # Mengisi record ke kolom yang sudah di buat tadi
+ 
+ 
+w.poly(parts=[[[1,3],[5,3]]], shapeType=shapefile.POLYLINE) # Membuat polyline
+
+w.save("soal6") 		# Untuk save menjadi shp file
+
+
+
+
+
+
+ 
+
+
+
+
+
---
+++ 1164049_NURARKHAMIABATUBARA/7.py
@@ -0,0 +1,28 @@
+import shapefile 		# Mengimport modul shape file
+w=shapefile.Writer() 	# Mendeklarasi variabel
+w.shapeType 			# Menjalankan perintah dari pendeklarasian variabel
+
+w.field("kolom1","C") 	# Membuat kolom dengan tipe data character
+w.field("kolom2","C") 	# Membuat kolom dengan tipe data character
+
+w.record("ngek","satu") # Mengisi record ke kolom yang sudah di buat tadi
+  
+w.poly(parts=[[[1,3],[5,3],[1,2],[5,2]]],shapeType=shapefile.POLYLINE) # Membuat polyline
+
+w.save("soal7") 		# Untuk save menjadi shp file
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
