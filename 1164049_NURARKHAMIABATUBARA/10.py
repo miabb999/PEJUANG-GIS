46c7ccbbf42b180d81c3cb098bc1ab38fc1288ff
NUR ARKHAMIA BATUBARA <37619556+miabb999@users.noreply.github.com>
2018-10-24 20:00:44 +0700

    Add files via upload
    HARI KE 6

---
+++ 1164049_NURARKHAMIABATUBARA/10.py
@@ -0,0 +1,39 @@
+import shapefile   		# Mengimport modul shape file
+w=shapefile.Writer()    # Mendeklarasi variabel
+w.shapeType             # Menjalankan perintah dari pendeklarasian variabel
+
+w.field("Nama Bidang","Blackpink")		# Membuat kolom dengan tipe data character
+w.field("Nama Bidang","Blackpink")
+
+w.record("Segitiga Sama sisi","lisa")	# Mengisi record ke kolom yang sudah di buat tadi
+w.record("Segitiga Sama sisi","jisoo")	# Mengisi record ke kolom yang sudah di buat tadi
+w.record("Segitiga Sama sisi","jennie")	# Mengisi record ke kolom yang sudah di buat tadi
+w.record("Segitiga Sama sisi","rose")	# Mengisi record ke kolom yang sudah di buat tadi
+
+w.poly(parts=[[[1,1],[5,1],[3,4],[1,1]]],shapeType=shapefile.POLYGON)	# Membuat polygon
+w.poly(parts=[[[1,-1],[5,-1], [3,-4],[1,-1]]],shapeType=shapefile.POLYGON)	# Membuat polygon
+w.poly(parts=[[[-6,-1],[-2,-1],[-4,-4],[-6,-1]]],shapeType=shapefile.POLYGON)	# Membuat polygon
+w.poly(parts=[[[-6,1],[-2,1],[-4,4],[-6,1]]],shapeType=shapefile.POLYGON)	# Membuat polygon
+
+w.save("soal 10")	# Untuk save menjadi shp file
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
